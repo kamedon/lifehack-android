3688d458277746ca8c585cc8bfe4a10ff4af3a66
Kamei Hidetoshi <h_kamei@menue.co.jp>
2016-03-25 15:57:52 +0900

    install Crashlytics

--- app/src/main/java/com/kamedon/todo/KamedonApplication.java
+++ app/src/main/java/com/kamedon/todo/KamedonApplication.java
@@ -3,6 +3,8 @@ package com.kamedon.todo;
 import android.app.Application;
 
 import com.squareup.leakcanary.LeakCanary;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by h_kamei on 2016/03/25.
@@ -11,6 +13,7 @@ public class KamedonApplication extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         LeakCanary.install(this);
     }
 }
