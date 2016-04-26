8f6e92b845f68e66d922e1567a97b87d648a9752
kamedon <hkamei06@gmail.com>
2016-04-27 08:58:30 +0900

    定数のリファクタリング

--- app/src/main/java/com/kamedon/todo/domain/api/TodoClientConfig.kt
+++ app/src/main/java/com/kamedon/todo/domain/api/TodoClientConfig.kt
@@ -1,13 +1,18 @@
 package com.kamedon.todo.domain.api
 
+import com.kamedon.todo.BuildConfig
 import com.kamedon.todo.infra.repository.UserRepository
-import com.kamedon.todo.util.XUserAgentAuthorizationUtil
+import okhttp3.internal.Util
 
 /**
  * Created by kamedon on 4/3/16.
  */
 class TodoClientConfig(val userService: UserRepository) {
-    fun xAgentKey() = XUserAgentAuthorizationUtil.header()
-    fun xAgentToken() = XUserAgentAuthorizationUtil.token()
+    fun xAgentKey() = BuildConfig.X_USER_AGENT_AUTHORIZATION_KEY
+    fun xAgentToken() =token(BuildConfig.X_USER_AGENT_AUTHORIZATION_TOKEN)
     fun userToken() = userService.getApiKey();
+
+    private fun token(key: String): String {
+        return Util.md5Hex(key);
+    }
 }
